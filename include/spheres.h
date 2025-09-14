c859516ee35c8d6a53fa6097752c2342a678e056
Sarait Hernandez <saraithernandez0205@gmail.com>
2025-09-14 19:38:57 +0200

    fixing some normitte errors

--- include/spheres.h
+++ include/spheres.h
@@ -50,20 +50,21 @@ typedef struct s_sphere
 /*
 ** Sphere creation and operations
 */
-t_sphere	sphere_create(void);
-t_sphere	sphere_set_transform(t_sphere s, t_matrix transform);
-t_xs		sphere_intersect(t_sphere *s, t_ray r);
-t_tuple		sphere_normal_at(t_sphere *s, t_tuple world_point);
+t_sphere		sphere_create(void);
+t_sphere		sphere_set_transform(t_sphere s, t_matrix transform);
+t_xs			sphere_intersect(t_sphere *s, t_ray r);
+t_tuple			sphere_normal_at(t_sphere *s, t_tuple world_point);
 
 /*
 ** Intersection utilities
 */
-t_intersection	intersection_create(double t, void *obj);
+
 t_xs			xs_create(int count);
 void			intersections_destroy(t_xs *xs);
 t_xs			intersections_add(t_xs xs, t_intersection i);
-t_intersection	intersections_hit(t_xs xs);
 int				intersections_count(t_xs xs);
+t_intersection	intersection_create(double t, void *obj);
 t_intersection	intersections_get(t_xs xs, int index);
+t_intersection	intersections_hit(t_xs xs);
 
 #endif
