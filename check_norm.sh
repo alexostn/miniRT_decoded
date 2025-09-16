fc79aa6e3e5167eb86300ea275018bf1e052e61e
alexostn <alexejosten@gmail.com>
2025-09-16 20:40:38 +0200

    modified check_norm.sh you can use any subfolder excepcion it goes to .tar archive, checks Norm, brings them back structured

--- check_norm.sh
+++ check_norm.sh
@@ -2,36 +2,62 @@
 # "Instead of running norminette ., just run your new script from the project root:
 # ./check_norm.sh
 
-#!/bin/bash
+# --- 1. Universal Ignore List ---
+# Just add any file or directory paths here.
+# tar will automatically preserve their structure.
+IGNORE_LIST=(
+	"tests"
+	"libft"
+	"mlx-linux"
+	"src/spheres" #remove the rule and Norminette it please thank You:)
+	"src/rays" #remove the rule and Norminette it please thank You:)
+	# "path/to/another/file.c"
+	# You can add any other directory to ignore using the same pattern
+)
 
-# Name of the temporary directory. It will be created one level up.
-TMP_DIR="norm_tmp"
+# The name of the temporary archive file.
+TMP_ARCHIVE="../norm_archive.tar"
 
-# Create the temporary directory in the parent directory (../)
-mkdir -p "../$TMP_DIR"
+# --- 2. Archiving and Removing ---
+# The script will find which items from the list actually exist
+# and add them to the command.
+files_to_move=()
+for item in "${IGNORE_LIST[@]}"; do
+	if [ -e "$item" ]; then
+		files_to_move+=("$item")
+	fi
+done
 
-echo "-> Temporarily moving ignored items (tests, libft, mlx-linux, book_tests.c)...to ../$TMP_DIR"
-# Move the directories to be ignored one level up.
-# "|| true" is needed so the script doesn't stop if a directory is missing.
-mv tests "../$TMP_DIR/" || true
-mv libft "../$TMP_DIR/" || true
-mv mlx-linux "../$TMP_DIR/" || true
-mv book_tests.c "../$TMP_DIR/" || true
-# You can add any other directory to ignore using the same pattern
+# If there are files to move, create an archive and delete the originals.
+if [ ${#files_to_move[@]} -gt 0 ]; then
+	echo "-> Temporarily archiving and removing ignored items..."
+	# tar -c (create archive) -f (file name)
+	# --remove-files does all the magic: archives the files, then deletes them.
+	tar -cf "$TMP_ARCHIVE" --remove-files "${files_to_move[@]}"
+else
+    echo "-> No ignored items found to move."
+fi
+
 
 echo "-------------------------------------"
 echo "Running norminette on the project..."
 echo "-------------------------------------"
 
-# Run norminette. It won't see the files in ../norm_tmp now.
 norminette .
 
 echo "-------------------------------------"
-echo "Moving directories back..."
-# Move the directories back from the parent temporary directory
-mv "../$TMP_DIR"/* . || true
 
-# Remove the temporary directory
-rmdir "../$TMP_DIR"
 
-echo "Done."
+# --- 3. Restoring and Cleaning Up ---
+# If the temporary archive exists, extract it and then delete it.
+if [ -f "$TMP_ARCHIVE" ]; then
+    echo "-> Restoring items to their original locations..."
+    # tar -x (extract) -f (from file)
+    # The files will be restored to their original paths.
+    tar -xf "$TMP_ARCHIVE"
+    # Remove the archive file itself.
+    rm "$TMP_ARCHIVE"
+    echo "Done."
+else
+    echo "-> No archive found to restore from."
+fi
