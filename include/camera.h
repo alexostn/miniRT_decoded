b4bde9d0f719c9fb03aeb32476a93b64eb64c1bb
alexostn <alexejosten@gmail.com>
2025-10-07 22:56:57 +0200

    📸 view_transform_default & 👁 visual demos for different cases world_render_main.c with ∞consecutive file names for each render

--- include/camera.h
+++ include/camera.h
@@ -6,7 +6,7 @@
 /*   By: oostapen <oostapen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 22:22:50 by oostapen          #+#    #+#             */
-/*   Updated: 2025/10/07 22:39:35 by oostapen         ###   ########.fr       */
+/*   Updated: 2025/10/07 22:56:35 by oostapen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,12 @@
 # include "tuples.h"
 # include "matrices.h"
 
+/*
+[left.x    left.y    left.z    0]
+[true_up.x true_up.y true_up.z 0]
+[-fwd.x    -fwd.y    -fwd.z    0]
+[0         0         0         1]
+*/
 t_matrix	view_transform(t_tuple from, t_tuple to, t_tuple up);
 
 #endif
